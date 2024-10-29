1e8e9efd4cf13ead5b9f4e3c510da01f32cb05b6
bagsjay666 <137271851+bagsjay666@users.noreply.github.com>
2024-10-29 14:28:50 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+if(! "webkitSpeechRecognition" in window){
+    alert("not avaliable");
+};
+
+const textarea = document.querySelector("#textarea");
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList;
+const SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
+
+const recognition = new SpeechRecognition();
+
+recognition.continuous = true;
+recognition.lang = "en-US";
+recognition.interimResults = true;
+
+/* recognition.onstart = ()=>{
+    console.log('start');
+};
+
+recognition.onend = ()=>{
+    console.log('end');
+}; */
+
+recognition.onresult = function (event){
+    let textResult = '';
+
+    for(let i=0;i<event.results.length; i++){
+        textResult += event.results[i][0].transcript;
+    };
+    textarea.innerHTML = textResult;
+};
+
+recognition.onend = function(){
+    recognition.start();
+};
+
+document.querySelector("#start").addEventListener("click", ()=>{
+    recognition.start();
+})
+
+document.querySelector("#stop").addEventListener("click", ()=>{
+    recognition.stop();
+})
