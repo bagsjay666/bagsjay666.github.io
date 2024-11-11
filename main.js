87428c216a748c8bcfa650444597d886a66ad30c
bagsjay666 <137271851+bagsjay666@users.noreply.github.com>
2024-11-11 15:49:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ if(! "webkitSpeechRecognition" in window){
 };
 
 const btn_start = document.querySelector("#btn-start");
+const btn_stop = document.querySelector("#btn-stop");
 const textarea = document.querySelector("#textarea");
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -23,14 +24,20 @@ recognition.onend = ()=>{
     console.log('end');
 }; */
 
+let printtext = (textResult)=>{
+    textarea.innerText = textResult;
+    textarea.scrollTop = textarea.scrollHeight;
+};
+
 recognition.onresult = function (event){
     let textResult = '';
 
     for(let i=0;i<event.results.length; i++){
         textResult += event.results[i][0].transcript;
     };
-    textarea.innerText = textResult;
-    textarea.scrollTop = textarea.scrollHeight;
+    if(typeof printtext === 'function'){
+        printtext(textResult);
+    };
 };
 
 recognition.onend = function(){
@@ -39,9 +46,15 @@ recognition.onend = function(){
 };
 
 btn_start.addEventListener("click", ()=>{
+    console.log("started");
     recognition.start();
     /* btn_start.parentNode.removeChild(btn_start); */
-})
+});
+
+btn_stop.addEventListener("click", ()=>{
+    console.log("forced to stop printing text");
+    printtext = undefined;
+});
 
 /* document.querySelector("#stop").addEventListener("click", ()=>{
     recognition.stop();
